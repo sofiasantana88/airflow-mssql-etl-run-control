7ccbbdcc0cba9b2570bd327c0027ba28ac566ba8
Sofia Santana <sofia.santana@bellevuecollege.edu>
2026-02-02 20:55:02 -0800

    Move SQL scripts into sql folder

---
+++ sql/ETL_RunEnd.sql
@@ -0,0 +1,28 @@
+﻿USE [ZAGI_DW]
+GO
+/****** Object:  StoredProcedure [dbo].[ETL_RunEnd]    Script Date: 1/12/2026 7:11:42 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ETL_RunEnd]
+	@AirflowRunID varchar(50) = ''
+	,@DagID varchar(50) = '0'
+	,@EndTime datetime2(7) = NULL
+	,@Status varchar(10) = 'Started'
+	,@ErrorMessage varchar(50) = ''
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        INSERT INTO ETL_RunLog (AirflowRunID, DagID, EndTime, ErrorMessage, Status)
+        VALUES (@AirflowRunID, @DagID, @EndTime, @ErrorMessage, @Status);
+    END TRY
+    BEGIN CATCH
+        INSERT INTO ETL_RunLog (AirflowRunID, DagID, EndTime, ErrorMessage, Status)
+        VALUES (@AirflowRunID, @DagID, @EndTime, @ErrorMessage, 'Failed');
+        THROW;
+    END CATCH
+END;
+
